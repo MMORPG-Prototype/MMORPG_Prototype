3ebe00217aeca5dfbbae79f940840b49723308a3
Adrian Pankiewicz <farami11@tlen.pl>
2017-03-30 21:10:35 +0200

    Project setup

---
+++ Client/desktop/src/pl/mmorpg/protptype/client/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package pl.mmorpg.protptype.client.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import pl.mmorpg.protptype.client.GameClient;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new GameClient(), config);
+	}
+}
---
+++ Server/desktop/src/pl/mmorpg/protptype/server/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package pl.mmorpg.protptype.server.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import pl.mmorpg.protptype.server.GameServer;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new GameServer(), config);
+	}
+}
