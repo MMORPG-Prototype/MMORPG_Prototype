904bf5103631ddd01962f07da9d13ce31cab775e
Adrian Pankiewicz <farami11@tlen.pl>
2017-04-07 12:29:16 +0200

    general commit

--- Client/core/src/pl/mmorpg/prototype/client/communication/PacketsMaker.java
+++ Client/core/src/pl/mmorpg/prototype/client/communication/PacketsMaker.java
@@ -1,21 +1,10 @@
 package pl.mmorpg.prototype.client.communication;
 
-import pl.mmorpg.prototype.client.objects.GameObject;
-import pl.mmorpg.prototype.clientservercommon.packets.ObjectCreationPacket;
 import pl.mmorpg.prototype.clientservercommon.packets.ObjectRemovePacket;
 import pl.mmorpg.prototype.clientservercommon.packets.movement.ObjectRepositionPacket;
 
 public class PacketsMaker
 {
-    public static ObjectCreationPacket makeCreationPacket(GameObject object)
-    {
-        ObjectCreationPacket packet = new ObjectCreationPacket();
-        packet.id = object.getId();
-        packet.identifier = object.getIdentifier();
-        packet.x = object.getX();
-        packet.y = object.getY();
-        return packet;
-    }
 
     public static ObjectRemovePacket makeRemovalPacket(int id)
     {
--- Client/core/src/pl/mmorpg/prototype/client/input/PlayInputContinuousHandler.java
+++ Client/core/src/pl/mmorpg/prototype/client/input/PlayInputContinuousHandler.java
@@ -2,7 +2,7 @@ package pl.mmorpg.prototype.client.input;
 
 import com.esotericsoftware.kryonet.Client;
 
-import pl.mmorpg.prototype.clientservercommon.packets.entities.UserCharacterDataPacket;
+import pl.mmorpg.prototype.client.objects.Player;
 import pl.mmorpg.prototype.clientservercommon.packets.movement.MoveDownPacket;
 import pl.mmorpg.prototype.clientservercommon.packets.movement.MoveLeftPacket;
 import pl.mmorpg.prototype.clientservercommon.packets.movement.MoveRightPacket;
@@ -11,12 +11,12 @@ import pl.mmorpg.prototype.clientservercommon.packets.movement.MoveUpPacket;
 public class PlayInputContinuousHandler extends InputProcessorAdapter
 {
     private Client client;
-    private UserCharacterDataPacket character;
+    private Player player;
 
-    public PlayInputContinuousHandler(Client client, UserCharacterDataPacket character)
+    public PlayInputContinuousHandler(Client client, Player player)
     {
         this.client = client;
-        this.character = character;
+        this.player = player;
     }
 
     public class WKeyHandler implements KeyHandler
@@ -24,9 +24,12 @@ public class PlayInputContinuousHandler extends InputProcessorAdapter
         @Override
         public void handle()
         {
-            MoveUpPacket packet = new MoveUpPacket();
-            packet.id = character.getId();
-            client.sendTCP(packet);
+            if(player.isPictureInSamePosition())
+            {
+                MoveUpPacket packet = new MoveUpPacket();
+                packet.id = player.getId();
+                client.sendTCP(packet);
+            }
         }
     }
 
@@ -35,9 +38,12 @@ public class PlayInputContinuousHandler extends InputProcessorAdapter
         @Override
         public void handle()
         {
-            MoveLeftPacket packet = new MoveLeftPacket();
-            packet.id = character.getId();
-            client.sendTCP(packet);
+            if(player.isPictureInSamePosition())
+            {
+                MoveLeftPacket packet = new MoveLeftPacket();
+                packet.id = player.getId();
+                client.sendTCP(packet);
+            }           
         }
     }
 
@@ -46,9 +52,12 @@ public class PlayInputContinuousHandler extends InputProcessorAdapter
         @Override
         public void handle()
         {
-            MoveDownPacket packet = new MoveDownPacket();
-            packet.id = character.getId();
-            client.sendTCP(packet);
+            if(player.isPictureInSamePosition())
+            {
+                MoveDownPacket packet = new MoveDownPacket();
+                packet.id = player.getId();
+                client.sendTCP(packet);
+            }           
         }
     }
 
@@ -57,9 +66,12 @@ public class PlayInputContinuousHandler extends InputProcessorAdapter
         @Override
         public void handle()
         {
-            MoveRightPacket packet = new MoveRightPacket();
-            packet.id = character.getId();
-            client.sendTCP(packet);
+            if(player.isPictureInSamePosition())
+            {
+                MoveRightPacket packet = new MoveRightPacket();
+                packet.id = player.getId();
+                client.sendTCP(packet);
+            }
         }
     }
 }
