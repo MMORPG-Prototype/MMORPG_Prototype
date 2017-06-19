05809f7a46bd692a18a750adac5d0393e829f524
Adrian Pankiewicz <farami11@tlen.pl>
2017-06-19 10:46:20 +0200

    External folder with assets is no longer needed

--- Client/core/src/pl/mmorpg/prototype/client/resources/Assets.java
+++ Client/core/src/pl/mmorpg/prototype/client/resources/Assets.java
@@ -8,6 +8,12 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.TreeSet;
+import java.util.regex.Pattern;
+
+import org.reflections.Reflections;
+import org.reflections.scanners.ResourcesScanner;
+import org.reflections.util.ClasspathHelper;
+import org.reflections.util.ConfigurationBuilder;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.assets.AssetManager;
@@ -30,7 +36,7 @@ import pl.mmorpg.prototype.clientservercommon.Settings;
 
 public class Assets
 {
-	private static String assetsPath = "assets";
+	private static String assetsPath = "";
 	private static Map<String, Class<?>> classTypes = new HashMap<String, Class<?>>();
 	private static Set<String> ignoredExtensions = new TreeSet<>();
 	private static AssetManager assets = new AssetManager();
@@ -40,19 +46,18 @@ public class Assets
 	private static List<CustomStage> stages = new LinkedList<>();
 
 	static
-    {
+	{
 		loadSeveralStages();
 		assets.setLoader(TiledMap.class, new TmxMapLoader());
 		addClassTypes();
 		addIgnoredExtendsions();
-		loadSkins(assetsPath);
+		loadSkins();
 		loadOthers();
-    }
-	
-	
+	}
+
 	private static void loadSeveralStages()
 	{
-		for(int i=0; i<7; i++)
+		for (int i = 0; i < 7; i++)
 		{
 			CustomStage stage = createStage();
 			stage.setUsed(false);
@@ -60,20 +65,16 @@ public class Assets
 		}
 	}
 
-
 	private static void addClassTypes()
-    {
+	{
 		classTypes.put("png", Texture.class);
-        classTypes.put("jpg", Texture.class);
-        classTypes.put("bmp", Texture.class);
-        classTypes.put("gif", Texture.class);
-        classTypes.put("mp3", Music.class);
-        classTypes.put("ogg", Sound.class);
-        classTypes.put("tmx", TiledMap.class);
-    }
-
-
-	
+		classTypes.put("jpg", Texture.class);
+		classTypes.put("bmp", Texture.class);
+		classTypes.put("gif", Texture.class);
+		classTypes.put("mp3", Music.class);
+		classTypes.put("ogg", Sound.class);
+		classTypes.put("tmx", TiledMap.class);
+	}
 
 	private static void addIgnoredExtendsions()
 	{
@@ -91,36 +92,39 @@ public class Assets
 		ignoredExtensions.add("json");
 	}
 
-	private static void loadSkins(String path)
+	private static void loadSkins()
 	{
-		FileHandle[] files = Gdx.files.internal(path).list();
-		for (FileHandle file : files)
+		Set<String> a = new Reflections(new ConfigurationBuilder().setUrls(ClasspathHelper.forJavaClassPath())
+				.setScanners(new ResourcesScanner())).getResources(Pattern.compile("(.*?).json"));
+		for (String s : a)
 		{
-			if (file.isDirectory())
-				loadSkins(file.path());
-			else if (getExtension(file.path()).equals("json"))
-				skins.put(file.path(), new Skin(file));
+			skins.put(s, new Skin(Gdx.files.classpath(s)));
 		}
 
 	}
 
 	public static void loadOthers()
-    {
+	{
 		Collection<FileHandle> fileHandles = new ArrayList<>();
-        fileHandles = loadFromSubdirectories(assetsPath, fileHandles);
-		for (FileHandle file : fileHandles)
-			assets.load(file.path(), getClassFromPath(file.path()));
+		Set<String> a = new Reflections(new ConfigurationBuilder().setUrls(ClasspathHelper.forJavaClassPath())
+				.setScanners(new ResourcesScanner())).getResources(Pattern.compile("(.*?).(gif|png|tmx|jpg)"));
 
-        assets.finishLoading();
-    }
+		System.out.println("Size: " + a.size());
+		for (String s : a)
+		{
+			System.out.println(s);
+			assets.load(s, getClassFromPath(s));
+		}
 
+		assets.finishLoading();
+	}
 
 	private static Collection<FileHandle> loadFromSubdirectories(String path, Collection<FileHandle> fileHandles)
 	{
 		FileHandle[] files = Gdx.files.internal(path).list();
 		for (FileHandle file : files)
 		{
-			if(file.isDirectory())
+			if (file.isDirectory())
 				fileHandles = loadFromSubdirectories(file.path(), fileHandles);
 			else if (!ignoredExtensions.contains(getExtension(file.path())))
 				fileHandles.add(file);
@@ -129,54 +133,54 @@ public class Assets
 	}
 
 	private static Class<?> getClassFromPath(String path)
-    {
-        String extension = getExtension(path);
-        if (classTypes.containsKey(extension))
-            return classTypes.get(extension);
-        else
-            throw new UnknownExtensionException(extension);
-    }
+	{
+		String extension = getExtension(path);
+		if (classTypes.containsKey(extension))
+			return classTypes.get(extension);
+		else
+			throw new UnknownExtensionException(extension);
+	}
 
 	private static String getExtension(String path)
-    {
-        int extensionStartIndex = path.lastIndexOf('.') + 1;
-        return path.substring(extensionStartIndex);
-    }
+	{
+		int extensionStartIndex = path.lastIndexOf('.') + 1;
+		return path.substring(extensionStartIndex);
+	}
 
 	public static <T> T get(String fileName)
-    {
+	{
 		T asset;
 		try
 		{
-			asset = assets.get(assetsPath + '/' + fileName);
+			asset = assets.get(assetsPath + fileName);
 		} catch (GdxRuntimeException e)
 		{
-			asset = (T) skins.get(assetsPath + '/' + fileName);
+			asset = (T) skins.get(assetsPath + fileName);
 		}
-		if(asset == null)
+		if (asset == null)
 			throw new UnloadedAssetException(fileName);
 		return asset;
-    }
+	}
 
 	public static <T> T get(String fileName, Class<T> classType)
-    {
-        return assets.get(assetsPath + '/' + fileName, classType);
-    }
+	{
+		return assets.get(assetsPath + fileName, classType);
+	}
 
 	public static void dispose()
-    {
-        assets.dispose();
-        font.dispose();
+	{
+		assets.dispose();
+		font.dispose();
 		stages.clear();
 		batch.dispose();
 		for (Skin skin : skins.values())
 			skin.dispose();
-    }
+	}
 
 	public static BitmapFont getFont()
-    {
-        return font;
-    }
+	{
+		return font;
+	}
 
 	public static Stage getStage()
 	{
@@ -192,11 +196,9 @@ public class Assets
 		return newStage;
 	}
 
-
 	private static CustomStage createStage()
 	{
-		return new CustomStage(new ScalingViewport(Scaling.stretch, Settings.GAME_WIDTH, Settings.GAME_HEIGHT),
-				batch);
+		return new CustomStage(new ScalingViewport(Scaling.stretch, Settings.GAME_WIDTH, Settings.GAME_HEIGHT), batch);
 	}
 
 	public static SpriteBatch getBatch()
@@ -205,31 +207,32 @@ public class Assets
 	}
 
 	private static class UnknownExtensionException extends GameException
-    {
-        public UnknownExtensionException(String extension)
-        {
+	{
+		public UnknownExtensionException(String extension)
+		{
 			super("Extension " + extension + " is not recognized");
-        }
-    }
-	
+		}
+	}
+
 	private static class UnloadedAssetException extends GameException
 	{
 		public UnloadedAssetException(String fileName)
-        {
+		{
 			super("Unloaded resource: " + fileName);
-        }
+		}
 	}
 
 	public static class Textures
 	{
 		public final static String MAIN_CHAR = "MainChar.png";
+
 		public static class Items
 		{
 			public static final String ITEM_FOLDER = "Items/";
 			public static final String SMALL_HEALTH_POTION = ITEM_FOLDER + "SmallHealthPotion.png";
 			public static final String SMALL_MANA_POTION = ITEM_FOLDER + "SmallManaPotion.png";
 		}
-		
+
 		public static class Map
 		{
 			public static final String MAP_FOLDER = "MAP/";
