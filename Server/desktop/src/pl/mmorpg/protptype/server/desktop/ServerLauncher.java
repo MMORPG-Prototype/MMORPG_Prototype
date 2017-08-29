12b368dc192c9f2552abffb902ae547ccb6d3717
Adrian Pankiewicz <farami11@tlen.pl>
2017-08-29 22:15:14 +0200

    Test

--- Server/desktop/src/pl/mmorpg/protptype/server/desktop/ServerLauncher.java
+++ Server/desktop/src/pl/mmorpg/protptype/server/desktop/ServerLauncher.java
@@ -12,7 +12,7 @@ import pl.mmorpg.prototype.server.GameServer;
 @SpringBootApplication(scanBasePackages="pl.mmorpg.prototype")
 public class ServerLauncher
 {
-	public static void main(String[] args) 
+	public static void main(String[] args)  
 	{
 		SpringApplication.run(ServerLauncher.class, args);
 		
