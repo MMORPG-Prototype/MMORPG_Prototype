3d25b1980c8f0fb1ae5bfbdfa3ea7296042cb2ab
Pankiev <s160381@student.pg.gda.pl>
2017-09-05 12:57:34 +0200

    bug fix

--- Server/core/src/pl/mmorpg/prototype/server/helpers/Authenticator.java
+++ Server/core/src/pl/mmorpg/prototype/server/helpers/Authenticator.java
@@ -2,8 +2,6 @@ package pl.mmorpg.prototype.server.helpers;
 
 import java.util.Map;
 
-import javax.persistence.NoResultException;
-
 import pl.mmorpg.prototype.SpringContext;
 import pl.mmorpg.prototype.clientservercommon.packets.AuthenticationPacket;
 import pl.mmorpg.prototype.clientservercommon.packets.AuthenticationReplyPacket;
@@ -14,64 +12,59 @@ import pl.mmorpg.prototype.server.exceptions.NotAuthenticatedException;
 
 public class Authenticator
 {
-	private Map<Integer, UserInfo> authenticatedUsers;
-	private AuthenticationPacket packet;
+    private Map<Integer, UserInfo> authenticatedUsers;
+    private AuthenticationPacket packet;
 
-	private AuthenticationReplyPacket replyPacket;
-	private User authenticatedUser;
+    private AuthenticationReplyPacket replyPacket;
+    private User authenticatedUser;
 
-	public Authenticator(Map<Integer, UserInfo> authenticatedUsers, AuthenticationPacket packet)
-	{
-		this.authenticatedUsers = authenticatedUsers;
-		this.packet = packet;
-	}
+    public Authenticator(Map<Integer, UserInfo> authenticatedUsers, AuthenticationPacket packet)
+    {
+        this.authenticatedUsers = authenticatedUsers;
+        this.packet = packet;
+    }
 
+    public AuthenticationReplyPacket tryAuthenticating()
+    {
+        replyPacket = new AuthenticationReplyPacket();
 
-	public AuthenticationReplyPacket tryAuthenticating()
-	{
-		replyPacket = new AuthenticationReplyPacket();
-		User user = null;
-		try
-		{
-			UserRepository userRepo = SpringContext.getBean(UserRepository.class);
-			user = userRepo.findByUsername(packet.username);
-		}
-		catch(NoResultException e)
-		{
-			replyPacket.message = "Wrong username";
-			return replyPacket;
-		}
-		
-		if (user.getPassword().compareTo(packet.password) == 0)
-		{
-			if (authenticatedUsers.containsKey(user.getId()))
-				replyPacket.message = "User is already logged in!";
-			else
-			{
-				replyPacket.userId = user.getId();
-				replyPacket.isAuthenticated = true;
-				authenticatedUser = user;
-			}
-		} 
-		else
-			replyPacket.message = "Wrong password";
-		return replyPacket;
-	}
+        UserRepository userRepo = SpringContext.getBean(UserRepository.class);
+        User user = userRepo.findByUsername(packet.username);
+        if(user == null)
+        {
+            replyPacket.message = "Wrong username";
+            return replyPacket;
+        }
 
-	public boolean isAuthenticated()
-	{
-		return replyPacket.isAuthenticated;
-	}
+        if (user.getPassword().compareTo(packet.password) == 0)
+        {
+            if (authenticatedUsers.containsKey(user.getId()))
+                replyPacket.message = "User is already logged in!";
+            else
+            {
+                replyPacket.userId = user.getId();
+                replyPacket.isAuthenticated = true;
+                authenticatedUser = user;
+            }
+        } else
+            replyPacket.message = "Wrong password";
+        return replyPacket;
+    }
 
-	public User getUser()
-	{
-		if (!isAuthenticated())
-			throw new NotAuthenticatedException();
-		return authenticatedUser;
-	}
+    public boolean isAuthenticated()
+    {
+        return replyPacket.isAuthenticated;
+    }
 
-	public AuthenticationReplyPacket getReplyPacket()
-	{
-		return replyPacket;
-	}
+    public User getUser()
+    {
+        if (!isAuthenticated())
+            throw new NotAuthenticatedException();
+        return authenticatedUser;
+    }
+
+    public AuthenticationReplyPacket getReplyPacket()
+    {
+        return replyPacket;
+    }
 }
