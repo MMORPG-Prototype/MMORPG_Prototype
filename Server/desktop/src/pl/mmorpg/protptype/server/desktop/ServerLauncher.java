3449225a0d2a3098be7e2939f0702b4976d6a189
Pankiev <farami11@tlen.pl>
2019-04-17 14:39:22 +0200

    Upgrade libgdx version

--- Server/desktop/src/pl/mmorpg/protptype/server/desktop/ServerLauncher.java
+++ Server/desktop/src/pl/mmorpg/protptype/server/desktop/ServerLauncher.java
@@ -1,11 +1,9 @@
 package pl.mmorpg.protptype.server.desktop;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import pl.mmorpg.prototype.clientservercommon.Settings;
 import pl.mmorpg.prototype.server.GameServer;
 
@@ -15,21 +13,10 @@ public class ServerLauncher
 
 	public static void main(String[] args)  
 	{
-		initializeSpring(args);
+		SpringApplication.run(ServerLauncher.class, args);
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		config.width = Settings.WINDOW_WIDTH;
 		config.height = Settings.WINDOW_HEIGHT; 
 		new LwjglApplication(new GameServer(), config);
-	}
-
-	private static void initializeSpring(String[] args)
-	{
-		try {
-			SpringApplication.run(ServerLauncher.class, args);
-		}
-		catch(Exception e){
-			new MySqlLauncher().launch();
-			SpringApplication.run(ServerLauncher.class, args);
-		}
 	}
 }
